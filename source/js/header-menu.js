30e7ebb6aa83cd8f0ab57f73fa9b21afe88648e1
anyakul <anyakulikova111@gmail.com>
2020-08-05 16:03:13 +0300

    Делаю все страницы сайта

--- source/js/header-menu.js
+++ source/js/header-menu.js
@@ -1,16 +1,19 @@
 'use strict';
 
 var navToggle = document.querySelector('.header__toggle');
-var navMain = document.querySelector('.header__menu');
+var navMain = document.querySelector('.header__nav');
 
 navToggle.classList.remove('header__menu--nojs');
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('header__menu--opened')) {
-    navMain.classList.remove('header__menu--opened');
-    navMain.classList.add('header__menu--closed');
+  if (navMain.classList.contains('header__nav--opened')) {
+    navMain.classList.remove('header__nav--opened');
+    navMain.classList.add('header__nav--closed');
   } else {
-    navMain.classList.add('header__menu--opened');
-    navMain.classList.remove('header__menu--closed');
+    navMain.classList.add('header__nav--opened');
+    navMain.classList.remove('header__nav--closed');
   }
 });
+
+
+console.log(navToggle);
